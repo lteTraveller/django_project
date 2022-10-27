8999c54f649472a0709886e015f629a8b4a0bf7e
lteTraveller <ayobamisodiq68@gmail.com>
2022-10-27 17:05:51 +0100

    created models and attributes according to week 5 task

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+    def __str__(self):
+        if self.first_name:
+            return f'{self.first_name} {self.last_name}'
+        return  self.first_name
+
+class Song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    date_released = models.DateField(default=datetime.today)
+    likes = models.IntegerField()
+
+    def __str__(self):
+        return  self.title
+ 
+class Lyric(models.Model):
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
+
+    def __str__(self):
+        return  self.content
